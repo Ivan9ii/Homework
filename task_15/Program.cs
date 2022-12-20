50fb4c5566bd0e223cfb94b821ee00309d44f0c6
Ivan Ivanov <alecs.911@mail.ru>
2022-12-21 00:38:31 +0700

    Задача №15

---
+++ task_15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ведите номер дня недели от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 6 || day == 7)
+Console.WriteLine("это выходной день");
+else if (day < 1 || day > 7)
+ Console.WriteLine("в недели всего 7 дней");
+else Console.WriteLine("это рабочий день");
