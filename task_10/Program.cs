b8860fd2315de5b557754fcec52d4bcceba77651
Ivan Ivanov <alecs.911@mail.ru>
2022-12-21 00:37:48 +0700

    Задача №10

---
+++ task_10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string stingnum = Convert.ToString(num);
+Console.WriteLine("вторая цифра:" + stingnum[1]);
