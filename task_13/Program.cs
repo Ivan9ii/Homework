92366e0060f94d4da7af806efb85bdf5f627b07f
Ivan Ivanov <alecs.911@mail.ru>
2022-12-21 00:38:14 +0700

    Задача №13

---
+++ task_13/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string stringnum = Convert.ToString(num);
+if(stringnum.Length > 2) Console.WriteLine($"третья цифра: {stringnum[2]}");
+else Console.WriteLine("это не трехзначное");
